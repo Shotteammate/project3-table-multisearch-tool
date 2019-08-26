005dc0279793e644109ee075bfd1c86f30c86a13
Tik San Pun <shotteammate@gmail.com>
2019-08-26 17:38:30 +0800

    Parse fetched data

--- src/App.js
+++ src/App.js
@@ -22,13 +22,27 @@ export class App extends Component {
     fetch(`https://raw.githubusercontent.com/mledoze/countries/master/countries.json`)
       // We get the API response and receive data in JSON format...
       .then(response => response.json())
-      // ...then we update the users state
-      .then(data =>
+      .then(data => {
+        let array = [];
+
+        for (let i = 0; i < data.length; i++) {
+          let entry = new Object();
+          //key value pairs
+          entry.country = data[i].name.official;
+          entry.capital = data[i].capital;
+          entry.region = data[i].region;
+          entry.subregion = data[i].subregion;
+          // get first child of the languages object
+          entry.language = data[i].languages[Object.keys(data[i].languages)[0]];
+
+          array[i] = entry;
+        }
         this.setState({
-          data: data,
+          data: array,
           isLoading: false,
-        })
-      )
+        });
+      })
+
       // Catch any errors we hit and update the app
       .catch(error => this.setState({ error, isLoading: false }));
   }
@@ -38,8 +52,8 @@ export class App extends Component {
 
     return (
       <div className="App">
-        <h1>testing</h1>
-       
+        <h1>Country/Capital Data Multi-Search Service</h1>
+
       </div>
     )
   }
